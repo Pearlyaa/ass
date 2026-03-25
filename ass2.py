ad658b6b434a11e99fed4132a53f46a7e3b6b58c
Pearlyaa <yaamary963@gmail.com>
2026-03-25 01:58:59 +0000

    Add files via upload

---
+++ ass2.py
@@ -0,0 +1,7 @@
+Hello, World!
+x = int(6)  #This is for an integer
+y = float(4)    #This is for a float
+print(50-25)   #This is subtraction👀💀
+print(34+45)    #This is addition
+print(34/17)     #This is division
+print(23*10)      #This is multiplication
