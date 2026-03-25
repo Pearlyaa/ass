d298537a71fe05a790e2acff6328af86ab590768
Pearlyaa <yaamary963@gmail.com>
2026-03-25 02:19:07 +0000

    Delete ass2.py

--- ass2.py
+++
@@ -1,7 +0,0 @@
-Hello, World!
-x = int(6)  #This is for an integer
-y = float(4)    #This is for a float
-print(50-25)   #This is subtraction👀💀
-print(34+45)    #This is addition
-print(34/17)     #This is division
-print(23*10)      #This is multiplication
